3db4c1b4a14d1762b1434adef6d3b2da5b0516e7
Colin-Paslay <148103002+Colin-Paslay@users.noreply.github.com>
2023-11-15 10:28:23 -0600

    Player and Hub
    Added Player, movement, animations, y-sorting, and a hub world, imported assets and set up an autotile for future use

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,16 @@
 extends CharacterBody2D
 @export var speed: float = 300.0
-func _physics_process(delta):
+@onready var animated_sprite_2d = $AnimatedSprite2D
+func handleInput():
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction*speed
+	velocity = direction * speed
+func updateAnimation():
+	if velocity.x < 0: animated_sprite_2d.play("walk west")
+	elif velocity.x > 0: animated_sprite_2d.play("walk east")
+	elif velocity.y > 0: animated_sprite_2d.play("walk south")
+	elif velocity.y < 0: animated_sprite_2d.play("walk north")
+	else: animated_sprite_2d.stop()
+func _physics_process(delta):
+	handleInput()
 	move_and_slide()
+	updateAnimation()
